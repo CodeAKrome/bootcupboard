e539d78346beaa1a3fc005b33b6e3d2fd3c641c7
kyle <kyleg.job@gmail.com>
2023-10-05 08:18:46 -0400

    The play's the thing

--- instructor-embedding/flair_nlp.py
+++ instructor-embedding/flair_nlp.py
@@ -50,9 +50,7 @@ class Nlp(object):
             mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
             return mm.read().decode()
 
-    def sentences(
-        self, file="artofwar.txt", query="What are the seven considerations?"
-    ):
+    def sentences(self, file="artofwar.txt"):
         """Break a file into lines by sentence."""
         print(f"Reading: {file}")
         text = self.mmap_file(file)
@@ -62,6 +60,57 @@ class Nlp(object):
         sentences = [Sentence(sent, use_tokenizer=True) for sent in sents]
         for sentence in sentences:
             print(sentence.text)
+
+    def play(self, file="artofwar.txt"):
+        """Break a play into lines.
+        Concattenate next line to this if this line starts with capitals."""
+        print(f"Reading: {file}")
+        text = self.mmap_file(file)
+        text = re.sub(r"\s+", " ", text)
+        sents = split_single(text)
+        sents = [sent for sent in sents if sent]
+        sentences = [Sentence(sent, use_tokenizer=True) for sent in sents]
+        hold = ''
+        for sentence in sentences:
+            txt = sentence.text
+            if re.match(r'^[A-Z\.]{3,}', txt):
+                hold = txt
+                continue
+            if hold:
+                print(f"{hold} {txt}")
+                hold = ''
+            else:
+                print(txt)
+
+    def playrag(self, file="artofwar.txt"):
+        """Break a play into lines.
+        Concattenate next line to this if this line starts with capitals."""
+        print(f"Reading: {file}")
+        text = self.mmap_file(file)
+        text = re.sub(r"\s+", " ", text)
+        sents = split_single(text)
+        sents = [sent for sent in sents if sent]
+        sentences = [Sentence(sent, use_tokenizer=True) for sent in sents]
+        buf = ''
+        actor = ''
+        i = 0
+        for sentence in tqdm(sentences):
+            txt = sentence.text
+            if re.match(r'^[A-Z\.]{3,}', txt):
+                actor = txt
+                buf = txt
+                continue
+            if buf:
+                txt = f"{buf} {txt}"
+                buf = ''
+            #print(txt)
+            coll.upsert(ids=[f"{file}:{i}"], metadatas=[{'player':actor}], documents=[txt])
+            i += 1
+
+
+    def peek(self):
+        """Show first 5 items of collection"""
+        return coll.peek()
 
     def rag(self, file="artofwar.txt"):
         """Break a file into lines by sentence and store it in chromadb."""
@@ -73,7 +122,8 @@ class Nlp(object):
         sentences = [Sentence(sent, use_tokenizer=True) for sent in sents]
         i = 0
         for sentence in tqdm(sentences):
-            coll.add(documents=[sentence.text], ids=[f"{file}:{i}"])
+            coll.upsert(documents=[sentence.text], ids=[f"{file}:{i}"])
+#            coll.add(documents=[sentence.text], ids=[f"{file}:{i}"])
             i += 1
 
     def query(self, q="What are the seven considerations?"):
