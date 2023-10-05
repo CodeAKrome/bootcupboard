675e59f36087c8e410a7191497b57f43ca64e5c2
kyle <kyleg.job@gmail.com>
2023-10-05 08:54:17 -0400

    play part detect

--- instructor-embedding/flair_nlp.py
+++ instructor-embedding/flair_nlp.py
@@ -61,26 +61,6 @@ class Nlp(object):
         for sentence in sentences:
             print(sentence.text)
 
-    def play(self, file="artofwar.txt"):
-        """Break a play into lines.
-        Concattenate next line to this if this line starts with capitals."""
-        print(f"Reading: {file}")
-        text = self.mmap_file(file)
-        text = re.sub(r"\s+", " ", text)
-        sents = split_single(text)
-        sents = [sent for sent in sents if sent]
-        sentences = [Sentence(sent, use_tokenizer=True) for sent in sents]
-        hold = ''
-        for sentence in sentences:
-            txt = sentence.text
-            if re.match(r'^[A-Z\.]{3,}', txt):
-                hold = txt
-                continue
-            if hold:
-                print(f"{hold} {txt}")
-                hold = ''
-            else:
-                print(txt)
 
     def playrag(self, file="artofwar.txt"):
         """Break a play into lines.
@@ -96,16 +76,35 @@ class Nlp(object):
         i = 0
         for sentence in tqdm(sentences):
             txt = sentence.text
-            if re.match(r'^[A-Z\.]{3,}', txt):
+            if re.match(r'^\w+\.$', txt):
                 actor = txt
                 buf = txt
                 continue
             if buf:
                 txt = f"{buf} {txt}"
                 buf = ''
-            #print(txt)
             coll.upsert(ids=[f"{file}:{i}"], metadatas=[{'player':actor}], documents=[txt])
             i += 1
+
+    def play(self, file="artofwar.txt"):
+        """Break a play into lines.
+        Concattenate next line to this if this line starts with capitals."""
+        print(f"Reading: {file}")
+        text = self.mmap_file(file)
+        text = re.sub(r"\s+", " ", text)
+        sents = split_single(text)
+        sents = [sent for sent in sents if sent]
+        sentences = [Sentence(sent, use_tokenizer=True) for sent in sents]
+        buf = ''
+        for sentence in sentences:
+            txt = sentence.text
+            if re.match(r'^\w+\.$', txt):
+                buf = txt
+                continue
+            if buf:
+                txt = f"{buf} {txt}"
+                buf = ''
+            print(txt)
 
 
     def peek(self):
