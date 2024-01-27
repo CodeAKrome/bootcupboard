a721d0f85b8f0974acbfd405b3499839952da32e
kyle gaspar <kyleg.job@gmail.com>
2024-01-27 10:03:43 -0500

    fire, heh. can pipe through llm

--- shed/OllamaStdin.py
+++ shed/OllamaStdin.py
@@ -1,29 +1,28 @@
 #!env python
 import ollama
 import sys
+import fire
 
-# args: system prompt, instructions
+def main(
+    query: str,
+    sysmsg="You are a helpful research assistant. You are accurate, concise and thorough. Your conclusions are supported by evidence.",
+    rag="Base your reply on the following information:"
+):
+    modelfile = f"FROM llama2\nSYSTEM {sysmsg}"
+    text = sys.stdin.read()
+    ollama.create(model="custom", modelfile=modelfile)
+    content = f"{query}\n{rag}\n{text}"
+    response = ollama.chat(
+        model="custom",
+        messages=[
+            {
+                "role": "user",
+                "content": content,
+            },
+        ],
+    )
+    print(response['message']['content'])
 
-modelfile = f"""
-FROM llama2
-SYSTEM {sys.argv[1]}
-"""
-text = sys.stdin.read() 
-ollama.create(model='custom', modelfile=modelfile)
-
-content = f"{sys.argv[2]}\nBase your reply on the following information:\n{text}\n"
-#print(f"Mod: {modelfile}\nCON: {content}")
-
-response = ollama.chat(model='custom', messages=[
-  {
-    'role': 'user',
-    'content': content,
-  },
-])
-print(f"---\n{response['message']['content']}\n")
-
-# no idea what this is supposed to do
-# bed = ollama.embeddings(model='llama2', prompt='They sky is blue because of rayleigh scattering')
-# print(bed)
-
-
+if __name__ == "__main__":
+    fire.Fire(main)
+    
