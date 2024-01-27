a2a9c61d74f90f0ab4cd754525257c8b569fe29f
kyle gaspar <kyleg.job@gmail.com>
2024-01-26 20:24:27 -0500

    mps not supported yexy -> video. boo

---
+++ text2video/c_demo.py
@@ -0,0 +1,22 @@
+import torch
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers.utils import export_to_video
+
+if torch.backends.mps.is_available():
+    print ("MPS device HO!")
+else:
+    print ("MPS device not found.")
+    
+torch.set_default_device('mps')
+
+pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
+
+pipe = pipe.to("mps")
+
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+
+#pipe.enable_model_cpu_offload()
+
+prompt = "Spiderman is surfing"
+video_frames = pipe(prompt, num_inference_steps=25).frames
+video_path = export_to_video(video_frames)
