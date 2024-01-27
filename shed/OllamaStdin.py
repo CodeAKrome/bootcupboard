24b9d7165830ad446fd8736d8bb059c8e3b0d5c1
kyle gaspar <kyleg.job@gmail.com>
2024-01-27 10:10:42 -0500

    choose llm model base

--- shed/OllamaStdin.py
+++ shed/OllamaStdin.py
@@ -5,10 +5,11 @@ import fire
 
 def main(
     query: str,
+    llm="llama2",
     sysmsg="You are a helpful research assistant. You are accurate, concise and thorough. Your conclusions are supported by evidence.",
-    rag="Base your reply on the following information:"
+    rag="Base your reply on the following information:",
 ):
-    modelfile = f"FROM llama2\nSYSTEM {sysmsg}"
+    modelfile = f"FROM {llm}\nSYSTEM {sysmsg}"
     text = sys.stdin.read()
     ollama.create(model="custom", modelfile=modelfile)
     content = f"{query}\n{rag}\n{text}"
